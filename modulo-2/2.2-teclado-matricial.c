eb6a8802b5a5d26a5589805beda14f67f67cd1e2
Deivit Bonach <62313672+dbonach@users.noreply.github.com>
2023-06-05 15:38:53 -0300

    Add boiler plate

--- modulo-2/2.2-teclado-matricial.c
+++ modulo-2/2.2-teclado-matricial.c
@@ -1,1 +1,14 @@
+/*  EA076C
+  Módulo 2: Teclado matricial
 
+  Alunos:
+    Deivit Lopes Bonach     RA:166508
+    Vinicius Henrique Luiz  RA:245342
+*/
+
+void setup(void)  {
+}
+
+void loop(void) {
+
+}

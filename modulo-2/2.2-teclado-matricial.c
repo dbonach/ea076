5560ba5e30358a2ef4aee8070405ff6b2b108a46
Deivit Bonach <deivitlopes@gmail.com>
2023-06-08 20:14:16 -0300

    update 08/06

--- modulo-2/2.2-teclado-matricial.c
+++ modulo-2/2.2-teclado-matricial.c
@@ -6,38 +6,87 @@
     Vinicius Henrique Luiz  RA:245342
 */
 
-void setup(void)  {
-}
+/*
+Pin relation:
+  pino 11 = coluna 1
+  pino 12 = coluna 2
+  pino 13 = coluna 3
+  pino 10 = linha 1
+  pino 9 = linha 2
+  pino 8 = linha 3
+  pino 7 = linha 4
 
-void loop(void) {
-  switch (variável)
+  pin 7 to 10 input
+  pint 11 to 13 output
+
+  output:
+    columns[3]
+    activeLine
+    pressedKey
+    flagKeyWasPressed
+
+  input:
+    lines[4]
+
+*/
+#include <Arduino.h>
+
+/* Create variables */
+int columns[3] = {1, 1, 1};
+int activeLine;
+int pressedKey;
+int flagKeyWasPressed = 0;
+int lines[4] = {1, 1, 1, 1};
+int state = 0;
+
+void setup()
 {
-    /*pino 10 = linha 1
-      pino 9 = linha 2
-      pino 8 = linha 3
-      pino 7 = linha 4
-      */
-   case constante1:
-     //coloca coluna 1 em 0V e mantem as demais em 5V
-     // pino 11 = coluna 1
-     // pino 12 = coluna 2
-     // pino 13 = coluna 3
-     digitalWrite(11,LOW);
-     digitalWrite(12,HIGH);
-     digitalWrite(13,HIGH);
-     
-     if (digitalRead(10)){
-      
-     }
+    Serial.begin(9600);
+    pinMode(7, INPUT_PULLUP);
+    pinMode(8, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
+    pinMode(10, INPUT_PULLUP);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
 
-   break;
-
-   case constante2:
-     Instruções;
-   break;
-
-   default
-     Instruções;
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
 }
 
+void loop()
+{
+    switch (state)
+    { // state machine
+    case 0:
+        digitalWrite(11, LOW);
+
+        if (digitalRead(11) == 0)
+        {
+            Serial.println("apertou a tecla 1");
+        }
+        break;
+    case 1:
+        // do something
+        break;
+    case 2:
+        // do something
+        break;
+    case 3:
+        // do something
+        break;
+    case 4:
+        // do something
+        break;
+    case 5:
+        // do something
+        break;
+    case 6:
+        // do something
+        break;
+    case 7:
+        // do something
+        break;
+    }
 }

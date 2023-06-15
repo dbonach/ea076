ca168b4eb71f6d1377278e196090266efe219c5b
Deivit Bonach <deivitlopes@gmail.com>
2023-06-15 11:28:40 -0300

    refactor code

--- modulo-2/2.2-teclado-matricial.c
+++ modulo-2/2.2-teclado-matricial.c
@@ -1,92 +1,159 @@
 /*  EA076C
-  Módulo 2: Teclado matricial
+    Modulo 2: Teclado matricial
 
-  Alunos:
-    Deivit Lopes Bonach     RA:166508
-    Vinicius Henrique Luiz  RA:245342
+    Alunos:
+        Deivit Lopes Bonach     RA:166508
+        Vinicius Henrique Luiz  RA:245342
 */
 
-/*
-Pin relation:
-  pino 11 = coluna 1
-  pino 12 = coluna 2
-  pino 13 = coluna 3
-  pino 10 = linha 1
-  pino 9 = linha 2
-  pino 8 = linha 3
-  pino 7 = linha 4
-
-  pin 7 to 10 input
-  pint 11 to 13 output
-
-  output:
-    columns[3]
-    activeLine
-    pressedKey
-    flagKeyWasPressed
-
-  input:
-    lines[4]
-
-*/
 #include <Arduino.h>
-#include <avr/interrupt.h>
-/* Create variables */
-int columns[3] = {1, 1, 1};
-int activeLine;
-int pressedKey;
-int flagKeyWasPressed = 0;
-int lines[4] = {1, 1, 1, 1};
-int state = 0;
+
+// definicao de constantes
+#define ROWS 4
+#define COLS 3
+#define NO_ACTIVE_LINE 100
+#define TRUE 1
+#define FALSE 0
+
+// arrays que mapeiam os pinos do arduino para as linhas e colunas do teclado
+const char rowPins[ROWS] = {2, 3, 4, 5};
+const char colPins[COLS] = {6, 7, 8};
+
+// mapeamento do teclado para obtencao das teclas
+char keypad[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+// declaracao de variaveis globais
+char debouncePass = 0;
+char activeColumn = 0;
+char activeLine = NO_ACTIVE_LINE;
+char lastActiveLine = NO_ACTIVE_LINE;
+char debouncedLine = NO_ACTIVE_LINE;
+char flagDebounce = FALSE;
+
+// configuracao do timer1 para gerar uma interrupcao a cada 50ms
+void configuracao_Timer1(){
+    
+    // configura timer1 para operar no modo CTC (Clear Timer on Compare Match)
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCCR1B |= (1 << WGM12);
+    
+    // valor de comparacao que corresponde a 50ms considerando o prescaler de 1024 e clock de 16MHz
+    OCR1A = 781;
+    
+    // ativa interrupcao por comparacao com o registrador OCR1A
+    TIMSK1 |= (1 << OCIE1A);
+    
+    // configura o prescaler para 1024
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+}
 
 void setup()
 {
-    Serial.begin(9600);
-    pinMode(7, INPUT_PULLUP);
-    pinMode(8, INPUT_PULLUP);
-    pinMode(9, INPUT_PULLUP);
-    pinMode(10, INPUT_PULLUP);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
+    // desabilita a flag global de interrupcao por precaucao
+    cli();
 
-    digitalWrite(11, HIGH);
-    digitalWrite(12, HIGH);
-    digitalWrite(13, HIGH);
+    Serial.begin(9600); // set baud rate to 9600 bps
+
+    // mapeamento dos pinos correspondentes a linha e colunas do teclado
+    // colunas sao configuradas como saida e linhas como entrada com pull-up
+    // o acionamento de uma tecla faz com que a linha correspondente seja colocada em nivel baixo
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+
+    // coloca as colunas em nivel alto
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+
+    /* configura o timer1 para gerar interrupcoes a cada 50ms 
+       que serao utilizadas para validar o acionamento das teclas */
+    configuracao_Timer1();
+    
+    // habilita a flag global de interrupcao
+    sei();
 }
 
-void loop()
-{
-    switch (state)
-    { // state machine
-    case 0:
-        digitalWrite(11, LOW);
+// loop de varredura do teclado
+void loop() {
+    /* a logica para identificar se uma tecla foi acionada eh baixar o nivel
+       de uma coluna e verificar se alguma linha foi colocada em nivel baixo */
 
-        if (digitalRead(11) == 0)
-        {
-            Serial.println("apertou a tecla 1");
+    // coluna eh colocada em nivel baixo
+    digitalWrite(colPins[activeColumn], LOW);
+
+    // varre as linhas
+    for (int row = 0; row <= ROWS ; row++) {
+
+        // caso nao encontrou nenhuma linha ativa atribui o valor NO_ACTIVE_LINE
+        if (row == ROWS) {
+            activeLine = NO_ACTIVE_LINE;
+
+        // caso encontre uma linha ativa atribui o valor da linha
+        } else if (digitalRead(rowPins[row]) == LOW) {
+            activeLine = row;
+
+            // fluxo eh quebrado para evitar que a linha seja desativada pelo if anterior na ultima iteracao
+            break;
         }
-        break;
-    case 1:
-        // do something
-        break;
-    case 2:
-        // do something
-        break;
-    case 3:
-        // do something
-        break;
-    case 4:
-        // do something
-        break;
-    case 5:
-        // do something
-        break;
-    case 6:
-        // do something
-        break;
-    case 7:
-        // do something
-        break;
+    }
+
+    // coluna eh colocada em nivel alto
+    digitalWrite(colPins[activeColumn], HIGH);
+
+    // atualiza a coluna caso nao tenha encontrado nenhuma linha ativa e nao esteja em debounce
+    if (activeLine == NO_ACTIVE_LINE && flagDebounce == FALSE) {
+        updateColumn();
+    }
+}
+
+// interrupcao a cada 50ms
+ISR(TIMER1_COMPA_vect) {
+
+    // valida se uma tecla esta ativa consecutivamente no intervalo de 50ms
+    if (activeLine != NO_ACTIVE_LINE && flagDebounce == FALSE) {
+
+        if (activeLine == lastActiveLine) {
+            flagDebounce = TRUE;
+            debouncedLine = activeLine; // salva a linha que foi acionada
+        }
+
+    // validacao se a tecla foi solta
+    // eh feito duas passagens sendo um debounce para validar se a tecla foi solta
+    } else if (activeLine == NO_ACTIVE_LINE && flagDebounce == TRUE) {
+
+        debouncePass++;
+
+        // na segunda passagem os estados sao resetados e a tecla eh impressa
+        if (debouncePass >= 2) {
+
+            char pressedKey = keypad[debouncedLine][activeColumn];
+            Serial.println("Pressed key: " + String(pressedKey));
+
+            debouncePass = 0;
+            flagDebounce = FALSE;
+        }   
+    }
+
+    // atualiza a ultima linha ativa no intervalo de 50ms
+    lastActiveLine = activeLine;
+}
+
+// atualiza coluna ativa, variando de 0 a 2
+void updateColumn() {
+    activeColumn++;
+
+    if (activeColumn >= COLS) {
+        activeColumn = 0;
     }
 }
